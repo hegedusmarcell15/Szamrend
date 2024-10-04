45a97a17b79d91636fdcb2e098ea949480d3d5f9
hegedusmarcell15 <hegedusm@kkszki.hu>
2024-10-04 13:24:01 +0200

    Fejlesztés alatt

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szamrendszerek
+{
+    internal class Program
+    {
+        public static int szamolas()
+        {
+            int szam = 0;
+            Console.WriteLine($"Kérem az átváltandó számot: ");
+            szam = szam % 2;
+            return szam;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(szamolas());
+            Console.ReadKey();
+        }
+
+    }
+}
